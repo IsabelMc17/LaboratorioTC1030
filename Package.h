d0b2c4158c246cfa8c5e0a919acd8e531e34c527
IsabelMc17 <isabelmoreno.co@gmail.com>
2020-05-22 17:49:53 +0000

    Modified class

--- Package.h
+++ Package.h
@@ -1,3 +1,55 @@
 #ifndef PACKAGE_H
 #define PACKAGE_H
+#include <iostream>
+
+
+using namespace std;
+
+class package{
+protected:
+  char remitente[50];
+  char destinatario[50];
+  float kilos;
+  float costoKg;
+
+public:
+  package();
+  package(char remit, char dest, float kg, float c);
+  char getDestinatario();
+  char getRemitente();
+  float calCosto();
+};
+
+package::package(){
+  remitente = "Undefined";
+  destinatario = "Undefined";
+  kilos = 0;
+  costoKg = 0;
+}
+
+package::package(char remit, char dest, float kg, float c){
+  remitente = remit;
+  destinatario = dest;
+  kilos = kg;
+  costoKg = c;
+}
+
+char package::getDestinatario(){
+  return destinatario;
+}
+
+char package::getRemitente(){
+  return remitente;
+}
+
+float package::calCosto(){
+  float costoF;
+  costoF = kilos * costoKg;
+  return costoF;
+}
+
+
+
+
+
 
